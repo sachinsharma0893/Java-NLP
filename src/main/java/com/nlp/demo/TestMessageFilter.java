6e626caf3f92127fdd8231486eb67e206cbf8f75
Sachin <Sachinsharma0893@gmail.com>
2020-04-14 16:35:50 +0530

    added separate Class to test NLPService Class Execution

---
+++ src/main/java/com/nlp/demo/TestMessageFilter.java
@@ -0,0 +1,41 @@
+package com.nlp.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class TestMessageFilter {
+
+	public static void main(String[] args) {
+		Map<String, Set<String>> map = NlpService.getFilteredMap(getSampleArrayList());
+		map.entrySet().stream().forEach(mapContent -> {
+			System.out.println(" map content is::::: " + mapContent);
+		});
+	}
+
+	private static List<String> getSampleArrayList() {
+		List<String> stringSet = new ArrayList<String>();
+		stringSet.add("its is grateful beautiful friends are fulfilling an incredibly joyful accomplishment.");
+		stringSet.add("What an truly terrible idea");
+		stringSet.add("How are you");
+		stringSet.add("VPN is slow");
+		stringSet.add("VPN is down");
+		stringSet.add("VPN is worst");
+		stringSet.add("I am Software Engineer");
+		stringSet.add("I am Software Developer");
+		stringSet.add("I am Engineer");
+		stringSet.add("i am facing VPN slowness");
+		stringSet.add("i faced issue where VPN is performing slow");
+		stringSet.add("i am facing VPN slowness");
+		stringSet.add("i am getting issues related to VPN slowness");
+		stringSet.add("there are VPN slowness issues");
+		stringSet.add("How are you");
+		stringSet.add("how are you doing");
+		stringSet.add("how do you do");
+		stringSet.add("You are bad guy");
+		stringSet.add("You are not good guy");
+		return stringSet;
+	}
+
+}
